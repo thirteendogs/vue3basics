98b415ec68242a9511699d2ee126c7e50beea623
thirteendogs <daniel.nova.girbau@gmail.com>
2022-10-23 20:59:56 +0200

    add reaction timer

---
+++ reaction-timer/src/components/Block.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="block" @click="stopTimer" v-if="showBlock">click me</div>
+</template>
+
+<script>
+export default {
+  props: ["delay"],
+  data() {
+    return {
+      showBlock: false,
+      time: null,
+      reactionTime: 0,
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showBlock = true;
+      this.startTimer();
+    }, this.delay);
+  },
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.reactionTime += 10;
+      }, 10);
+    },
+    stopTimer() {
+      clearInterval(this.timer);
+      this.$emit("end", this.reactionTime);
+    },
+  },
+};
+</script>
+
+<style>
+.block {
+  width: 400px;
+  border-radius: 20px;
+  background-color: #0faf87;
+  color: white;
+  text-align: center;
+  padding-block: 100px;
+  margin: 40px auto;
+}
+</style>
