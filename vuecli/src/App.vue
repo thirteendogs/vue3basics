653dee0a6e7f9d86674f71025d65342140eeadaf
thirteendogs <daniel.nova.girbau@gmail.com>
2022-10-23 16:31:45 +0200

    add props to the modal component

--- vuecli/src/App.vue
+++ vuecli/src/App.vue
@@ -1,17 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <p>welcome..</p>
+  <div v-if="showModal">
+    <Modal :header="header" :text="text" theme="sale" @close="toggleModal" />
+  </div>
+  <button @click="toggleModal">Open Modal</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from "./components/Modal.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Modal },
+  data() {
+    return {
+      title: "My first Vue App",
+      header: "Sign up for you giveaway!",
+      text: "Grab your giveaway!",
+      showModal: false,
+    };
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+  },
+};
 </script>
 
 <style>
@@ -22,5 +37,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
---
+++ vuecli/src/components/Modal.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <h1>{{ header }}</h1>
+      <p>{{ text }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["header", "text", "theme"],
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.backdrop {
+  top: 0;
+  position: fixed;
+  background-color: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 100px;
+  background-color: white;
+  border-radius: 10px;
+}
+h1 {
+  color: teal;
+  border: none;
+  padding: 0;
+}
+
+.sale {
+  background-color: crimson;
+  color: white;
+}
+
+.sale h1 {
+  color: white;
+}
+</style>
