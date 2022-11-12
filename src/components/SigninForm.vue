0b162626de412a161763df233c0cc8f5a26f1e58
thirteendogs <daniel.nova.girbau@gmail.com>
2022-11-13 00:55:20 +0100

    feat: add login & logout forms

---
+++ src/components/SigninForm.vue
@@ -0,0 +1,27 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input v-model="email" type="email" placeholder="email" required />
+    <input v-model="password" type="password" placeholder="password" required />
+    <button>Log in</button>
+  </form>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    const email = ref("");
+    const password = ref("");
+
+    const handleSubmit = () => {
+      console.log(email.value, password.value);
+    };
+
+    return {
+      email,
+      password,
+      handleSubmit,
+    };
+  },
+};
+</script>
---
+++ src/components/SignupForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input
+      v-model="displayName"
+      type="text"
+      placeholder="display name"
+      required
+    />
+    <input v-model="email" type="email" placeholder="email" required />
+    <input v-model="password" type="password" placeholder="password" required />
+    <button>Sign up</button>
+  </form>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    const displayName = ref("");
+    const email = ref("");
+    const password = ref("");
+
+    const handleSubmit = () => {
+      console.log(displayName.value, email.value, password.value);
+    };
+
+    return {
+      displayName,
+      email,
+      password,
+      handleSubmit,
+    };
+  },
+};
+</script>
