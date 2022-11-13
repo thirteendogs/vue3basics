49ad0707889b837159bd202376f99a57614e2d4c
thirteendogs <daniel.nova.girbau@gmail.com>
2022-11-13 15:08:49 +0100

    feat: add Navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav>
+    <div>
+      <p>Hey there...display name</p>
+      <p class="email">Currently logged in as... email</p>
+    </div>
+    <button>Logout</button>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
